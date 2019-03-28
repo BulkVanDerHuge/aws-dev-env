7f082a2c23cd8db7fcaca33fe5895734bd1c1faa
BulkVanDerHuge <matthew.burnett@sophos.com>
2019-03-28 12:38:09 -0400

    Updated the script to handle Python 3 instead of Python 2.7.
    * Updated the Python version.
    * Added additional packages.
    * Added aptitude for Debian-based packages.
    * Removed yapsy.

--- init.sh
+++ init.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+ADD_PKGS=
 BITS=
 PKG_MGR=''
 
@@ -35,8 +36,10 @@ function set_package_manager {
 		PKG_MGR='yum'
 	elif [ -f /etc/debian_version ]; then
 		PKG_MGR='apt-get'
+        ADD_PKGS='aptitude'
 	elif [ -f /etc-lsb-release ]; then
 		PKG_MGR='apt-get'
+        ADD_PKGS='aptitude'
 	fi
 }
 
@@ -60,11 +63,11 @@ else
 fi
 
 # Install the base packages
-sudo $PKG_MGR install python27-numpy tmux
+sudo $PKG_MGR install python3 tmux $ADD_PKGS
 
 # Install the Python packages
 # TODO: This may need to be under sudo
-sudo pip install arrow boto3 flake8 autopep8 yapsy
+sudo pip install arrow boto3 flake8 autopep8
 
 VIM=
 VIM_AUTO_LOAD=
