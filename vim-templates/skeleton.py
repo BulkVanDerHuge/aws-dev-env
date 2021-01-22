e1b8be432e6d67992647e9530d5fccb1e87bea5f
Matthew Burnett <matthew.burnett@sophos.com>
2021-01-22 10:08:36 -0500

    Added new skeleton files.

---
+++ vim-templates/skeleton.py
@@ -0,0 +1,10 @@
+# Copyright 2021, Sophos Limited. All rights reserved.
+#
+# 'Sophos' and 'Sophos Anti-Virus' are registered trademarks of
+# Sophos Limited and Sophos Group. All other product and company
+# names mentioned are trademarks or registered trademarks of their
+# respective owners.
+
+if __name__ == "__main__":
+    # pylint: disable=no-value-for-parameter
+    # TODO Add start code here
