3252f119a29e6e80d44fa0bfdbbdbfad2d1ec8ae
Matthew Burnett <matthew.burnett@sophos.com>
2021-03-09 14:07:53 -0500

    Updated the bash skeleton file.
    * Added -? alias for help.
    * Alphabetized function names because I am me.

--- vim-templates/skeleton.sh
+++ vim-templates/skeleton.sh
@@ -9,16 +9,16 @@ script_dir=$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd -P)
 
 usage() {
     cat <<EOF
-Usage: $(basename "${BASH_SOURCE[0]}") [-h] [-v] [-f] -p param_value arg1 [arg2...]
+Usage: $(basename "${BASH_SOURCE[0]}") [-h|?] [-v] [-f] -p param_value arg1 [arg2...]
 
-XXX Script description here. XXX
+*** SCRIPT DESCRIPTION HERE ***
 
 Available options:
 
--h, --help      Print this help and exit
--v, --verbose   Print script debug info
--f, --flag      Some flag description
--p, --param     Some param description
+-h -?, --help      Print this help and exit
+-v,    --verbose   Print script debug info
+-f,    --flag      Some flag description
+-p,    --param     Some param description
 EOF
 	exit
 }
@@ -28,23 +28,15 @@ cleanup() {
 	# script cleanup here
 }
 
-setup_colors() {
-	if [[ -t 2 ]] && [[ -z "${NO_COLOR-}" ]] && [[ "${TERM-}" != "dumb" ]]; then
-		NOFORMAT='\033[0m' RED='\033[0;31m' GREEN='\033[0;32m' ORANGE='\033[0;33m' BLUE='\033[0;34m' PURPLE='\033[0;35m' CYAN='\033[0;36m' YELLOW='\033[1;33m'
-	else
-		NOFORMAT='' RED='' GREEN='' ORANGE='' BLUE='' PURPLE='' CYAN='' YELLOW=''
-	fi
-}
-
-msg() {
-	echo >&2 -e "${1-}"
-}
-
 die() {
 	local msg=$1
 	local code=${2-1} # default exit status 1
 	msg "$msg"
 	exit "$code"
+}
+
+msg() {
+	echo >&2 -e "${1-}"
 }
 
 parse_params() {
@@ -54,6 +46,7 @@ parse_params() {
 
 	while :; do
 		case "${1-}" in
+			-? | --help) usage ;;
 			-h | --help) usage ;;
 			-v | --verbose) set -x ;;
 			--no-color) NO_COLOR=1 ;;
@@ -77,6 +70,14 @@ parse_params() {
 	return 0
 }
 
+setup_colors() {
+	if [[ -t 2 ]] && [[ -z "${NO_COLOR-}" ]] && [[ "${TERM-}" != "dumb" ]]; then
+		NOFORMAT='\033[0m' RED='\033[0;31m' GREEN='\033[0;32m' ORANGE='\033[0;33m' BLUE='\033[0;34m' PURPLE='\033[0;35m' CYAN='\033[0;36m' YELLOW='\033[1;33m'
+	else
+		NOFORMAT='' RED='' GREEN='' ORANGE='' BLUE='' PURPLE='' CYAN='' YELLOW=''
+	fi
+}
+
 parse_params "$@"
 setup_colors
 
@@ -86,3 +87,4 @@ msg "${RED}Read parameters:${NOFORMAT}"
 msg "- flag: ${flag}"
 msg "- param: ${param}"
 msg "- arguments: ${args[*]-}"
+
